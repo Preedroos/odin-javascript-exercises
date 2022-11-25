56995658db15f320f15ff8ccdeebf0b447e4ff60
Preedroos <preedroos@gmail.com>
2022-11-24 22:10:06 -0400

    Add assignment request exercises from Fundamentals Part 4 lesson

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let c = ((f - 32) * 5) / 9;
+  return Math.abs(c - Math.trunc(c)) > 0 ? Number(c.toFixed(1)) : c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let f = (c * 9) / 5 + 32;
+  return Math.abs(f - Math.trunc(f)) > 0 ? Number(f.toFixed(1)) : f;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
